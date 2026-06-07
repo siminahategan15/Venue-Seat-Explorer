774b4a5ba2f3baab9f726c7a217ec3fe32d57cf7
Simina Hategan <siminahategan@gmail.com>
2026-06-07 22:40:56 +0300

    Added README and removed env data

--- Frontend_Venue-Seat-Explorer/src/environments/environment.ts
+++ Frontend_Venue-Seat-Explorer/src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000',
-  googleMapsApiKey: 'AIzaSyB7cEJtJQ4j_pDR5BdG1eXFCUqaFFdfelg',
+  googleMapsApiKey: 'FAKE_GOOGLE_MAPS_API_KEY_123456',
   firebaseConfig: {
-    apiKey: 'AIzaSyBnHa51hISCQcJsnYSd--H-nk5_hswMKhY',
+    apiKey: 'FAKE_FIREBASE_API_KEY_ABCDEF',
     authDomain: 'venue-seat-explorer.firebaseapp.com',
     projectId: 'venue-seat-explorer',
     storageBucket: 'venue-seat-explorer.firebasestorage.app',
-    messagingSenderId: '335584213339',
-    appId: '1:335584213339:web:f39d78ba2a951fd875427e',
-    measurementId: 'G-X73ERFDET7',
+    messagingSenderId: '123456789012',
+    appId: '1:123456789012:web:abcdef1234567890abcdef',
+    measurementId: 'G-FAKEMEASURE123',
   },
 };
